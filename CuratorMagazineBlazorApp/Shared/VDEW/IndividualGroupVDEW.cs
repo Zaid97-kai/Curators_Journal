0b23214ad043bb0dfa75f34b4c4ed8cacb205073
RanisKzn <ranis_2@mail.ru>
2023-02-17 10:53:49 +0300

    Did the addition, deletion, modification and viewing of the student.

---
+++ CuratorMagazineBlazorApp/Shared/VDEW/IndividualGroupVDEW.cs
@@ -0,0 +1,51 @@
+﻿using API.Models.Entities.Domains;
+using WebClient.Data.Services;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Components;
+
+namespace WebClient.Shared.VDEW
+{
+    public partial class IndividualGroupVDEW
+    {
+        [Parameter]
+        public Group? Group { get; set; }
+
+        [Inject]
+        public GroupService? GroupService { get; set; }
+
+        [Inject]
+        public UserService? UserService { get; set; }
+
+        public User? user = new();
+
+        private bool IsVisibleModalChangeGroupWindow { get; set; }
+
+        private bool IsVisibleDeleteModalWindow { get; set; }
+
+        public void ShowModalChangeGroupWindow()
+        {
+            IsVisibleModalChangeGroupWindow = !IsVisibleModalChangeGroupWindow;
+        }
+
+        public void ShowModalChangeGroupWindoParam(bool value)
+        {
+            IsVisibleModalChangeGroupWindow = value;
+        }
+
+        public void ShowDeleteModalWindow()
+        {
+            IsVisibleDeleteModalWindow = !IsVisibleDeleteModalWindow;
+        }
+
+        public void DeleteModalWindowParam(bool value)
+        {
+            IsVisibleDeleteModalWindow = value;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var user1 = GroupService?.GetGroupCuratorAsync(Group!.Id).Result.Result;
+            user = user1;
+        }
+    }
+}
