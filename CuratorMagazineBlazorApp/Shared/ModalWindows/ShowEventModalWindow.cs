e9e8ba9a3cad33f4b3a556288b4d1b03d91d674e
RanisKzn <ranis_2@mail.ru>
2023-03-06 20:47:58 +0300

    Added pages for adding, viewing, deleting and changing events

---
+++ CuratorMagazineBlazorApp/Shared/ModalWindows/ShowEventModalWindow.cs
@@ -0,0 +1,45 @@
+﻿using API.Models.Entities.Domains;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+
+namespace WebClient.Shared.ModalWindows
+{
+    public partial class ShowEventModalWindow
+    {
+        [Parameter]
+        public Event? Event { get; set; }
+
+        /// <summary>
+        /// Gets or sets the change visible.
+        /// </summary>
+        /// <value>The change visible.</value>
+        [Parameter]
+        public EventCallback<bool> ChangeVisible { get; set; }
+
+        [Parameter]
+        public bool Visible { get; set; }
+
+        private void OnFinish(EditContext editContext)
+        {
+            Console.WriteLine($"Success: {JsonConvert.SerializeObject(Event)}");
+        }
+
+        /// <summary>
+        /// Called when [finish failed].
+        /// </summary>
+        /// <param name="editContext">The edit context.</param>
+        private void OnFinishFailed(EditContext editContext)
+        {
+            Console.WriteLine($"Failed: {JsonConvert.SerializeObject(Event)}");
+
+        }
+
+        private void HandleCancel(MouseEventArgs e)
+        {
+            Console.WriteLine("e");
+            ChangeVisible.InvokeAsync(false);
+        }
+    }
+}
---
+++ CuratorMagazineBlazorApp/Shared/StatsCollect/StatsCollectionPage.cs
@@ -0,0 +1,133 @@
+﻿using API.Models.Entities.Domains;
+using WebClient.Models.States;
+using WebClient.Data.Services;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Components;
+
+namespace WebClient.Shared.StatsCollect
+{
+    public partial class StatsCollectionPage
+    {
+        [Parameter]
+        public Group? Group { get; set; }
+
+        [Parameter]
+        public User? CurrentUser { get; set; }
+
+        [Inject]
+        public RoleService? RoleService { get; set; }
+
+        [Inject]
+        public GroupService? GroupService { get; set; }
+
+        [Inject]
+        public UserService? UserService { get; set; }
+
+        [Inject]
+        public GroupEventService? GroupEventService { get; set; }
+
+        [Inject]
+        public EventService? EventService { get; set; }
+
+        private bool IsVisibleAddStudentModalWindow { get; set; }
+
+        private bool IsVisibleAddEventModalWindow { get; set; }
+
+        private List<Group>? _groups = new();
+
+        private List<Role>? _roles = new();
+
+        private List<User>? _users = new();
+
+        private List<Event>? _events = new();
+
+        private List<GroupEvent>? _groupEvents = new();
+
+        private Role _role;
+
+        public void ShowModalAddStudentWindow()
+        {
+            IsVisibleAddStudentModalWindow = !IsVisibleAddStudentModalWindow;
+        }
+
+        public void ModalAddStudentWindowParam(bool value)
+        {
+            IsVisibleAddStudentModalWindow = value;
+        }
+
+        public void ShowModalAddEventWindow()
+        {
+            IsVisibleAddEventModalWindow = !IsVisibleAddEventModalWindow;
+        }
+
+        public void ModalAddEventWindowParam(bool value)
+        {
+            IsVisibleAddEventModalWindow = value;
+        }
+
+        public void ShowImportWindow()
+        {
+            
+
+        }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            var group = await GroupService?.PostAsync()!;
+            _groups = JsonConvert.DeserializeObject<List<Group>>(group.Result.Items?.ToString() ?? string.Empty);
+
+            var users = await UserService?.PostAsync()!;
+            _users = JsonConvert.DeserializeObject<List<User>>(users.Result.Items?.ToString() ?? string.Empty);
+
+            var roles = await RoleService?.PostAsync()!;
+            _roles = JsonConvert.DeserializeObject<List<Role>>(roles.Result.Items?.ToString() ?? string.Empty);
+
+            var events = await EventService?.PostAsync()!;
+            _events = JsonConvert.DeserializeObject<List<Event>>(events.Result.Items?.ToString() ?? string.Empty);
+
+            //var groupEvents = await GroupEventService?.PostAsync()!;
+            //_groupEvents = JsonConvert.DeserializeObject<List<GroupEvent>>(groupEvents.Result.Items?.ToString() ?? string.Empty);
+
+            _role = _roles.FirstOrDefault(i => i.Name == "Curator");
+
+            Group = _groups.FirstOrDefault(i => i.Id == 7);
+            CurrentUser = _users.FirstOrDefault(i => i.GroupId == 7);
+
+            //_groupEvents = _groupEvents.Where(i => i.GroupId == Group.Id).ToList();
+
+            //var displayEvents = new List<Event>();
+            //foreach(var groupEvent in _groupEvents)
+            //{
+            //    foreach(var @event in _events)
+            //    {
+            //        if (groupEvent.Id == @event.Id)
+            //        {
+            //            displayEvents.Add(@event);
+            //        }
+            //    }
+            //}
+
+            //if(displayEvents != null)
+            //{
+            //    _events = displayEvents;
+            //}
+
+            if (Group.Users == null)
+            {
+                Group.Users = new();
+            }
+
+            if (Group.Users.Count == 0)
+            {
+                foreach (var item in _users)
+                {
+                    Group.Users.Add(item);
+                }
+            }
+
+
+        }
+    }
+}
